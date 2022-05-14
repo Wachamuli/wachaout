124ae984d9c309c498878f49451592c2ffbace64
Wachamuli <josemrr27@gmail.com>
2022-05-14 12:25:08 -0600

    The support page is done

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,3 +1,85 @@
-function Support() {}
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
+import Button from "components/buttons/Button";
+import Title from "components/miscellaneous/Title";
+
+import styles from "styles/Support.module.css";
+
+function Support() {
+  return (
+    <div className={`section ${styles.section}`}>
+      <Title text="Support" />
+
+      <div className={styles.watermark}>
+        <Image
+          src="/resources/heart.svg"
+          alt="Image not found"
+          width={800}
+          height={800}
+        />
+      </div>
+
+      <div className={styles.textContainer}>
+        <h1 className={styles.subtitle}>
+          You can give me your money if you just like my work
+        </h1>
+        <p>without the need to work for you.</p>
+      </div>
+
+      <div className={styles.supportOptions}>
+        <div className={styles.supportOptionsContainer}>
+          <Button className={styles.btn}>
+            <Image
+              className={styles.btn}
+              width={270}
+              height={80}
+              src="/resources/become_a_patron_button.webp"
+              alt="Image not found"
+            />
+          </Button>
+        </div>
+
+        <div className={styles.supportOptionsContainer}>
+          <Button className={styles.btn}>
+            <Image
+              className={styles.btn}
+              width={270}
+              height={80}
+              src="/resources/checkout-logo-large-en.png"
+              alt="Image not found"
+            />
+          </Button>
+        </div>
+
+        <div className={styles.supportOptionsContainer}>
+          <Button className={styles.btn}>
+            <Image
+              width={270}
+              height={80}
+              src="/resources/bmc-button.png"
+              alt="Image not found"
+            />
+          </Button>
+        </div>
+      </div>
+
+      <label className={styles.moreBtn}>
+        More support methods
+        <FontAwesomeIcon
+          className={`hvr-icon ${styles.icon}`}
+          icon={faAngleDown}
+        />
+      </label>
+
+      <p>
+        Info: Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+        deleniti rerum a, sunt ipsa accusamus esse expedita provident eligendi
+        assumenda! Lorem ipsum dolor sit amet consectetur.
+      </p>
+    </div>
+  );
+}
 
 export default Support;
