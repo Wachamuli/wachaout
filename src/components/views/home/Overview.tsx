0f0726577473cc3e3927dc01882f4ceedca59469
Wachamuli <josemrr27@gmail.com>
2022-05-19 15:48:28 -0600

    Overview improvements

--- src/components/views/home/Overview.tsx
+++ src/components/views/home/Overview.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUpRightFromSquare, faPalette } from "@fortawesome/free-solid-svg-icons";
 
 import Button from "components/buttons/Button";
 import Title from "components/miscellaneous/Title";
+
 import styles from "styles/home/Overview.module.css";
 
 function Overview() {
@@ -17,36 +19,47 @@ function Overview() {
             alt=""
           />
           <div className={styles.content}>
-            {/* <div className={styles.logoContainer}>
-              <img className={styles.logo} src="#" alt="Image not found"/>
-            </div> */}
-            <h1 className={styles.serviceTitle}>Game Development</h1>
+            <div className={styles.logoContainer}>
+              <Image className={styles.logo} src="/placeholders/signature.png" alt="Image not found" width={300} height={150}/>
+            </div>
+            <h1 className={styles.serviceTitle}>
+              Illustrator | Drawing
+              <FontAwesomeIcon className={styles.icon} icon={faPalette}/>
+            </h1>
             <p className={styles.paragraph}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
               et repellat reprehenderit, repudiandae nesciunt asperiores enim
             </p>
             <Button className={styles.btn}>
               More info
-              <FontAwesomeIcon icon={faArrowAltCircleRight} className={styles.icon} />
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={styles.icon} />
             </Button>
           </div>
-          <div className={styles.secondaryImageContainer}>
-            <img
-              className={`${styles.image} ${styles.secondaryImage}`}
-              src="https://picsum.photos/id/139/800/500"
-              alt=""
-            />
-            <img
-              id={styles.imageBetween}
-              className={`${styles.image} ${styles.secondaryImage}`}
-              src="https://picsum.photos/id/117/800/500"
-              alt=""
-            />
-            <img
-              className={`${styles.image} ${styles.secondaryImage}`}
-              src="https://picsum.photos/id/9/800/500"
-              alt=""
-            />
+          <div className={styles.secondaryImages}>
+            <div className={styles.secondaryImageContainer}>
+              <label className={styles.title}>Lorem ipsum</label>
+              <img
+                className={`${styles.image} ${styles.secondaryImage}`}
+                src="https://picsum.photos/id/139/800/500"
+                alt=""
+              />
+            </div>
+            <div className={styles.secondaryImageContainer}>
+              <label className={styles.title}>Lorem ipsum</label>
+              <img
+                className={`${styles.image} ${styles.secondaryImage}`}
+                src="https://picsum.photos/id/117/800/500"
+                alt=""
+              />
+            </div>
+            <div className={styles.secondaryImageContainer}>
+              <label className={styles.title}>Lorem ipsum</label>
+              <img
+                className={`${styles.image} ${styles.secondaryImage}`}
+                src="https://picsum.photos/id/9/800/500"
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
