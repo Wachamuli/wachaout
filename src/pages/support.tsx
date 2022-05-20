092bf75aecce4eec6f0505b65ecb37063d59f1a6
Wachamuli <josemrr27@gmail.com>
2022-05-19 22:40:22 -0600

    Support page improvements

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -10,8 +10,6 @@ import styles from "styles/Support.module.css";
 function Support() {
   return (
     <div className={`section ${styles.section}`}>
-      <Title text="Support" />
-
       <div className={styles.watermark}>
         <Image
           src="/resources/heart.svg"
@@ -20,64 +18,63 @@ function Support() {
           height={800}
         />
       </div>
+      <div className={styles.sectionContainer}>
+        <Title text="Support" />
+        <div className={styles.textContainer}>
+          <h1 className={styles.subtitle}>
+            You can give me your money if you just like my work
+          </h1>
+          <p>without the need to work for you.</p>
+        </div>
 
-      <div className={styles.textContainer}>
-        <h1 className={styles.subtitle}>
-          You can give me your money if you just like my work
-        </h1>
-        <p>without the need to work for you.</p>
+        <div className={styles.supportOptions}>
+          <div className={styles.supportOptionsContainer}>
+            <Button className={styles.btn}>
+              <Image
+                className={styles.btn}
+                width={270}
+                height={80}
+                src="/resources/become_a_patron_button.webp"
+                alt="Image not found"
+              />
+            </Button>
+          </div>
+          <div className={styles.supportOptionsContainer}>
+            <Button className={styles.btn}>
+              <Image
+                className={styles.btn}
+                width={270}
+                height={80}
+                src="/resources/checkout-logo-large-en.png"
+                alt="Image not found"
+              />
+            </Button>
+          </div>
+          <div className={styles.supportOptionsContainer}>
+            <Button className={styles.btn}>
+              <Image
+                width={270}
+                height={80}
+                src="/resources/bmc-button.png"
+                alt="Image not found"
+              />
+            </Button>
+          </div>
+        </div>
+        <label className={styles.moreBtn}>
+          More support methods
+          <FontAwesomeIcon
+            className={`hvr-icon ${styles.icon}`}
+            icon={faAngleDown}
+          />
+        </label>
+        <p className={styles.info}>
+          <span className={styles.infoFormatter}>Info:</span> Lorem ipsum dolor sit amet 
+          consectetur adipisicing elit. Saepe deleniti rerum a, sunt ipsa 
+          accusamus esse expedita provident eligendi
+          assumenda! Lorem ipsum dolor sit amet consectetur.
+        </p>
       </div>
-
-      <div className={styles.supportOptions}>
-        <div className={styles.supportOptionsContainer}>
-          <Button className={styles.btn}>
-            <Image
-              className={styles.btn}
-              width={270}
-              height={80}
-              src="/resources/become_a_patron_button.webp"
-              alt="Image not found"
-            />
-          </Button>
-        </div>
-
-        <div className={styles.supportOptionsContainer}>
-          <Button className={styles.btn}>
-            <Image
-              className={styles.btn}
-              width={270}
-              height={80}
-              src="/resources/checkout-logo-large-en.png"
-              alt="Image not found"
-            />
-          </Button>
-        </div>
-
-        <div className={styles.supportOptionsContainer}>
-          <Button className={styles.btn}>
-            <Image
-              width={270}
-              height={80}
-              src="/resources/bmc-button.png"
-              alt="Image not found"
-            />
-          </Button>
-        </div>
-      </div>
-
-      <label className={styles.moreBtn}>
-        More support methods
-        <FontAwesomeIcon
-          className={`hvr-icon ${styles.icon}`}
-          icon={faAngleDown}
-        />
-      </label>
-
-      <p>
-        Info: Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-        deleniti rerum a, sunt ipsa accusamus esse expedita provident eligendi
-        assumenda! Lorem ipsum dolor sit amet consectetur.
-      </p>
     </div>
   );
 }
